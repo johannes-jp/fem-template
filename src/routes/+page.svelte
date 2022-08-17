bd4c73692234950a8cd131133b51d862d8054005
johannes-jp <johannes@solsten.com>
2022-08-17 11:32:29 +0300

    style config

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>Visit <a class="underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Footer from "$lib/components/Footer.svelte";
+
+</script>
+
+<!-- <h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
+<p>Visit <a class="underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+
+<Footer/>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,27 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+
+    extend: {
+      
+      screens: {
+        'Mobile': '375px',
+        'Desktop': '1440px',
+      },
+
+      colors: {
+        'White': 'hsl(0, 0%, 100%)',
+        'Light gray': 'hsl(212, 45%, 89%)',
+        'Grayish blue': 'hsl(220, 15%, 55%)',
+        'Dark blue': 'hsl(218, 44%, 22%)',
+      },
+
+      fontFamily: {
+        'Outfit': ['Outfit', 'sans-sarif'],
+      },
+  
+
+    },
   },
   plugins: [],
 }
