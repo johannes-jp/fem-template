ff9a0c622fcfbcd86a35b44f87f3c1c2c7a7d193
johannes-jp <johannes@solsten.com>
2022-08-17 11:43:21 +0300

    removed qr code files

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,21 +5,21 @@ module.exports = {
 
     extend: {
       
-      screens: {
-        'Mobile': '375px',
-        'Desktop': '1440px',
-      },
+      // screens: {
+      //   'Mobile': '375px',
+      //   'Desktop': '1440px',
+      // },
 
-      colors: {
-        'White': 'hsl(0, 0%, 100%)',
-        'Light gray': 'hsl(212, 45%, 89%)',
-        'Grayish blue': 'hsl(220, 15%, 55%)',
-        'Dark blue': 'hsl(218, 44%, 22%)',
-      },
+      // colors: {
+      //   'White': 'hsl(0, 0%, 100%)',
+      //   'Light gray': 'hsl(212, 45%, 89%)',
+      //   'Grayish blue': 'hsl(220, 15%, 55%)',
+      //   'Dark blue': 'hsl(218, 44%, 22%)',
+      // },
 
-      fontFamily: {
-        'Outfit': ['Outfit', 'sans-sarif'],
-      },
+      // fontFamily: {
+      //   'Outfit': ['Outfit', 'sans-sarif'],
+      // },
   
 
     },
